f06ea41041b18f6b8aeb7a7c35aac1b11b96e6c7
Allen <allen.m.lantz@gmail.com>
2021-11-15 17:36:00 -0600

    Start inventory events

---
+++ src/events/types/inventory/InventoryCloseEvent.ts
@@ -0,0 +1,34 @@
+import { Player } from '../../../player/Player';
+import { InventoryEvent } from './InventoryEvent';
+
+/**
+ * Represents a player related inventory event
+ */
+export class InventoryCloseEvent extends InventoryEvent {
+
+  public static getBukkitClasspath(): string {
+    return 'org.bukkit.event.inventory.InventoryCloseEvent';
+  }
+
+  /**
+   * Which button was pressed to initiate the enchanting.
+   */
+  public getPlayer(): Player {
+    return Player.fromJava(this.toJava().getPlayer());
+  }
+
+  /**
+   * Sets the cancellation state of this event
+   * @param cancel the cancellation state of this event
+   */
+  public setCancelled(cancel: boolean): void {
+    this.toJava().setCancelled(cancel);
+  }
+
+  /**
+   * Checks if this even has been cancelled
+   */
+  public isCancelled(): boolean {
+    return this.toJava().isCancelled();
+  }
+}
