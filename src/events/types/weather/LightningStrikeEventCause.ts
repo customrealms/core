49b0e36b8e13d2cc815abb69b020860e830c3753
Daniel Gonzalez <danielalexis.gp@gmail.com>
2021-11-25 23:16:50 -0700

    added lightningStrikeEventCause documentation

--- src/events/types/weather/LightningStrikeEventCause.ts
+++ src/events/types/weather/LightningStrikeEventCause.ts
@@ -1,10 +1,31 @@
 /** Causes that generate lightning strikes  */
 export enum LightningStrikeEventCause {
+    /**
+     * Triggered by the /summon command.
+     */
     COMMAND = 'COMMAND',
+    /**
+     * Triggered by a Plugin.
+     */
     CUSTOM = 'CUSTOM',
+    /**
+     * Triggered by a Spawner.
+     */
     SPAWNER = 'SPAWNER',
+    /**
+     * Triggered by a skeleton horse trap.
+     */
     TRAP = 'TRAP',
+    /**
+     * Triggered by an enchanted trident.
+     */
     TRIDENT = 'TRIDENT',
+    /**
+     * Unknown trigger.
+     */
     UNKNOWN = 'UNKNOWN',
+    /**
+     * Triggered by weather.
+     */
     WEATHER = 'WEATHER'
 }
