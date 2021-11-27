e30c477d7f8b98ce838450a0e30d1cdec7ec27a6
connerdouglass <conner.douglass@gmail.com>
2021-11-27 13:38:32 -0800

    Updated eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,7 +12,7 @@ module.exports = {
     rules: {
         '@typescript-eslint/no-namespace': 0,
         '@typescript-eslint/no-empty-interface': 0,
-        '@typescript-eslint/no-inferrable-types': 0,
+        '@typescript-eslint/no-inferrable-types': 1,
         '@typescript-eslint/ban-ts-comment': 1,
         '@typescript-eslint/no-empty-function': 1,
         '@typescript-eslint/no-explicit-any': 0,
