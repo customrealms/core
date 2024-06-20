833f06b4f08729736f7514d6fd165ff954c21d39
connerdouglass <conner.douglass@gmail.com>
2024-06-20 17:16:24 -0400

    feat: export Bukkit for convenience

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,9 @@ export * from './java';
 export * from './bukkit';
 export * from './globals';
 
+// Convenience exports of some common classes
+export const Bukkit = org.bukkit.Bukkit;
+
 export * from './command/CommandCall';
 export * from './command/ServerCommands';
 export * from './events/ServerEvents';
