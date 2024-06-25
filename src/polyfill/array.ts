429ce63eccd0bbe99746159d2f0b9144123d753a
Conner Douglass <conner.douglass@gmail.com>
2024-06-25 06:32:56 -0400

    feat: polyfill for Array.prototype.includes and String.prototype.includes (#43)

---
+++ src/polyfill/array.ts
@@ -0,0 +1,8 @@
+if (!Array.prototype.includes) {
+	Array.prototype.includes = function (
+		searchElement: any,
+		fromIndex?: number
+	): boolean {
+		return this.indexOf(searchElement, fromIndex) !== -1;
+	};
+}
