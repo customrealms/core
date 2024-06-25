8bc268830c551225ea36e12ea2b7de94659f1ea9
NexelOfficial <nathandiepeveen.nl@gmail.com>
2024-06-25 12:56:50 +0200

    Basic file writing and reading

--- src/java.ts
+++ src/java.ts
@@ -24,6 +24,54 @@ declare global {
             }
         }
         namespace util {
+            class Scanner {
+                constructor(source: io.File);
+                constructor(source: io.File, charsetName: string);
+                constructor(source: string);
+            
+                close(): void;
+                findInLine(pattern: string): string | null;
+                findWithinHorizon(pattern: string, horizon: number): string | null;
+                hasNext(): boolean;
+                hasNext(pattern: string): boolean;
+                hasNextBigDecimal(): boolean;
+                hasNextBigInteger(): boolean;
+                hasNextBigInteger(radix: number): boolean;
+                hasNextBoolean(): boolean;
+                hasNextByte(): boolean;
+                hasNextByte(radix: number): boolean;
+                hasNextDouble(): boolean;
+                hasNextFloat(): boolean;
+                hasNextInt(): boolean;
+                hasNextInt(radix: number): boolean;
+                hasNextLine(): boolean;
+                hasNextLong(): boolean;
+                hasNextLong(radix: number): boolean;
+                hasNextShort(): boolean;
+                hasNextShort(radix: number): boolean;
+                next(): string;
+                next(pattern: string): string;
+                nextBigInteger(): BigInteger;
+                nextBigInteger(radix: number): BigInteger;
+                nextBoolean(): boolean;
+                nextDouble(): number;
+                nextFloat(): number;
+                nextInt(): number;
+                nextInt(radix: number): number;
+                nextLine(): string;
+                nextLong(): number;
+                nextLong(radix: number): number;
+                nextShort(): number;
+                nextShort(radix: number): number;
+                radix(): number;
+                remove(): void;
+                reset(): Scanner;
+                skip(pattern: string): Scanner;
+                toString(): string;
+                useDelimiter(pattern: string): Scanner;
+                useRadix(radix: number): Scanner;
+            }
+
             interface Collection<T = any> extends java.lang.Iterable<T> {
                 add(value: T): boolean;
                 addAll(collection: java.util.Collection<T>): boolean;
@@ -124,6 +172,81 @@ declare global {
 
             namespace regex {
                 interface Pattern {}
+            }
+        }
+        namespace io {
+            class File {
+                constructor(parent: File, child: string);
+                constructor(pathName: string);
+                constructor(parent: string, child: string);
+                
+                canExecute(): boolean;
+                canRead(): boolean;
+                canWrite(): boolean;
+                compareTo(pathname: File): number;
+                createNewFile(): boolean;
+                static createTempFile(prefix: string, suffix: string): File;
+                static createTempFile(prefix: string, suffix: string, directory: File): File;
+                delete(): boolean;
+                deleteOnExit(): void;
+                exists(): boolean;
+                getAbsoluteFile(): File;
+                getAbsolutePath(): string;
+                getCanonicalFile(): File;
+                getCanonicalPath(): string;
+                getFreeSpace(): number;
+                getName(): string;
+                getParent(): string | null;
+                getParentFile(): File | null;
+                getPath(): string;
+                getTotalSpace(): number;
+                getUsableSpace(): number;
+                hashCode(): number;
+                isAbsolute(): boolean;
+                isDirectory(): boolean;
+                isFile(): boolean;
+                isHidden(): boolean;
+                lastModified(): number;
+                length(): number;
+                list(): string[] | null;
+                listFiles(): File[] | null;
+                static listRoots(): File[];
+                mkdir(): boolean;
+                mkdirs(): boolean;
+                renameTo(dest: File): boolean;
+                setExecutable(executable: boolean): boolean;
+                setExecutable(executable: boolean, ownerOnly: boolean): boolean;
+                setLastModified(time: number): boolean;
+                setReadable(readable: boolean): boolean;
+                setReadable(readable: boolean, ownerOnly: boolean): boolean;
+                setReadOnly(): boolean;
+                setWritable(writable: boolean): boolean;
+                setWritable(writable: boolean, ownerOnly: boolean): boolean;
+                toString(): string;
+            }
+
+            class FileWriter {
+                constructor(file: File);
+                constructor(file: File, append: boolean);
+                constructor(fileName: string);
+                constructor(fileName: string, append: boolean);
+
+                close(): void;
+                flush(): void;
+                getEncoding(): string | null;
+                write(c: number): void;
+                write(str: string, off?: number, len?: number): void;
+            }
+
+            class FileReader {
+                constructor(file: File);
+                constructor(fileName: string);
+
+                close(): void;
+                getEncoding(): string;
+                read(c: number): number;
+                read(cbuf: Uint8Array, off: number, len: number): number;
+                ready(): boolean;
             }
         }
     }
