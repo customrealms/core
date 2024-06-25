fbde20a8c60e337af804d9f0b3f1c716f837a705
connerdouglass <conner.douglass@gmail.com>
2024-06-25 05:30:39 -0400

    feat: polyfill for String.prototype.includes

---
+++ src/polyfill/string.ts
@@ -0,0 +1,5 @@
+if (!String.prototype.includes) {
+    String.prototype.includes = function(searchString: string, position?: number): boolean {
+        return this.indexOf(searchString, position) !== -1;
+    };
+}
