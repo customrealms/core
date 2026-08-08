d96bd20023c4553431a9e2f87bafb659955c68dd
connerdouglass <conner.douglass@gmail.com>
2026-08-08 10:53:45 -0400

    fix: remove 0.5 shift in sphere

--- src/region.ts
+++ src/region.ts
@@ -317,9 +317,9 @@ export class Shapes {
 			center.clone().add(radius, radius, radius),
 			center.clone().subtract(radius, radius, radius)
 		).filter((block) => {
-			const dx = block.getX() + 0.5 - center.getX();
-			const dy = block.getY() + 0.5 - center.getY();
-			const dz = block.getZ() + 0.5 - center.getZ();
+			const dx = block.getX() - center.getX();
+			const dy = block.getY() - center.getY();
+			const dz = block.getZ() - center.getZ();
 			return dx * dx + dy * dy + dz * dz <= radius * radius;
 		});
 	}
